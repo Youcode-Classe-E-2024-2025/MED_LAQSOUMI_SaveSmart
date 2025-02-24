5e2b8f42efd6094ad41b789fa3af4fff50169996
medlaq777 <m.laqsoumi@gmail.com>
2025-02-24 15:26:58 +0000

    fix username conflict on user modal also on the create user functionality in the authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -16,6 +16,7 @@ class AuthController extends Controller
     public function createUser()
     {
         $validated = request()->validate([
+            'username' => 'required|string|unique:users|max:55|min:3',
             'name' => 'required|string|max:55|min:3',
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|string|min:8|max:255',
