139c10012bc781a0a9fa7f3c64b28464efbc6db2
medlaq777 <m.laqsoumi@gmail.com>
2025-03-06 13:07:35 +0000

    fix all profile issues

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -27,19 +27,12 @@ class ProfileController extends Controller
         }
     }
 
-
     public function create()
     {
-        $user = Auth::user();
-        $profileCount = Profile::where('user_id', $user->id)->count();
-        
-        if ($profileCount >= 4) {
-            return redirect()->route('profile.manage', ['id' => $user->id])
-                ->with('error', 'You have reached the maximum limit of 4 profiles per account.');
-        }
-        
-        return view('profile.create');
+        $profile = Profile::all();
+        return view('profile.create', ['profiles' => $profile]);
     }
+
 
     public function store(Request $request)
     {
@@ -50,16 +43,17 @@ class ProfileController extends Controller
             return redirect()->route('profile.manage', ['id' => $user->id])
                 ->with('error', 'You have reached the maximum limit of 4 profiles per account.');
         }
-        
         $profile = new Profile();
+        $profile->user_id = Auth::id();
         $profile->name = $request->name;
         $profile->description = $request->description;
         $profile->pin = $request->pin;
         $profile->avatar = $request->avatar;
         $profile->user_id = $user->id;
         $profile->save();
-        return redirect()->route('profile', ['id' => $profile->id]);
+        return redirect()->route('profile.manage', ['id', $profile->id])->with('success', 'Profil créé avec succès !');
     }
+
 
     public function show(string $id)
     {
@@ -79,28 +73,28 @@ class ProfileController extends Controller
     }
 
     
-    public function update(Request $request, string $id)
-    {
-        $request->validate([
-            'name' => 'required',
-            'description' => 'required',
-            'pin' => 'required',
-            'avatar' => 'required'
-        ]);
-        $profile = Profile::find($id);
-        $profile->name = $request->name;
-        $profile->description = $request->description;
-        $profile->pin = $request->pin;
-        $profile->avatar = $request->avatar;
-        $profile->save();
-        return redirect()->route('profile.manage', ['id' => $profile->id])->with('success', 'Profile updated successfully');
-    }
+    // public function update(Request $request, string $id)
+    // {
+    //     $request->validate([
+    //         'name' => 'required',
+    //         'description' => 'required',
+    //         'pin' => 'required',
+    //         'avatar' => 'required'
+    //     ]);
+    //     $profile = Profile::find($id);
+    //     $profile->name = $request->name;
+    //     $profile->description = $request->description;
+    //     $profile->pin = $request->pin;
+    //     $profile->avatar = $request->avatar;
+    //     $profile->save();
+    //     return redirect()->route('profile.manage', ['id' => $profile->id])->with('success', 'Profile updated successfully');
+    // }
 
     
     public function destroy(string $id)
     {
-        $profile = Profile::find($id);
-        $profile->delete();
-        return redirect()->route('profile.manage', ['id' => $profile->id])->with('success', 'Profile deleted successfully');
+        $ProfileId = Profile::find($id);
+        $ProfileId->delete();
+        return redirect()->route('profile', ['id' => $ProfileId->id])->with('success', 'Profile deleted successfully');
     }
 }
